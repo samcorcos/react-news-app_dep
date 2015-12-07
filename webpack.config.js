20458a08559da95537d94de30429c31216891e1b
Sam Corcos <sam@corcos.io>
2015-12-07 09:46:23 -0800

    more configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,20 @@
 module.exports = {
-  context: __dirname + "/app",
-  entry: {
-    javascript: "./app.jsx",
-    html: "./index.html"
-  },
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+		'webpack/hot/dev-server',
+		'./app/app.jsx'
+  ],
   output: {
     filename: "bundle.js",
-    path: __dirname + "/dist"
+    path: __dirname + "/dist",
+    publicPath: '/static/'
   },
   module: {
     loaders: [
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        loaders: ["react-hot", "babel-loader"]
-      },
-      {
-        test: /\.html$/,
-        loader: "file?name=[name].[ext]"
-      }
+      { test: /\.jsx$/, exclude: /node_modules/, loaders: ["react-hot", "babel-loader"] },
+      { test: /\.html$/, loader: "file?name=[name].[ext]" },
+      { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' }
     ]
   }
 }
